51b344a3f9d5e3aaf7fda834d48ffdeca143f694
istamal <dagalaev@gmail.com>
2020-10-08 10:25:42 +0300

    Updates

---
+++ precache-manifest.6b2a021e0d0410f56e93e0c7abae1410.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef9d7dbdf6e440666085325149480e7d",
+    "url": "/contacts-demo/index.html"
+  },
+  {
+    "revision": "57c5fa0d864b45c06b95",
+    "url": "/contacts-demo/static/css/2.4b6e3ef2.chunk.css"
+  },
+  {
+    "revision": "9bc78bb3e94b91cdc3c8",
+    "url": "/contacts-demo/static/css/main.2496db1d.chunk.css"
+  },
+  {
+    "revision": "57c5fa0d864b45c06b95",
+    "url": "/contacts-demo/static/js/2.6b5d43e3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contacts-demo/static/js/2.6b5d43e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bc78bb3e94b91cdc3c8",
+    "url": "/contacts-demo/static/js/main.17d46f69.chunk.js"
+  },
+  {
+    "revision": "5287bfd662760c45a816",
+    "url": "/contacts-demo/static/js/runtime-main.8c6c5c5f.js"
+  }
+]);
